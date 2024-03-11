7776ee2cab64d256f836b13af2718328e862d299
Mohammed785 <mohammednady128@gmail.com>
2024-03-11 19:26:23 +0200

    respond to ping

--- app/server.go
+++ app/server.go
@@ -6,14 +6,20 @@ import (
 	"net"
 )
 
+func handleConnection(conn net.Conn) {
+	conn.Write([]byte("+PONG\r\n"))
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		log.Fatalln("Failed to bind to port 6379")
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		log.Fatal("Error accepting connection: ", err.Error())
 	}
+	handleConnection(conn)
+
 }
