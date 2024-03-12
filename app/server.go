829fc7b0cfdf7c4776f3bf891cb56d840ee1c0b1
Mohammed785 <mohammednady128@gmail.com>
2024-03-12 12:16:28 +0200

    handle multiple clients

--- app/server.go
+++ app/server.go
@@ -8,16 +8,18 @@ import (
 
 func handleConnection(conn net.Conn) {
 	data := make([]byte, 2048)
-	tmp := make([]byte,512)
+	defer conn.Close()
 	for {
-		n,_:=conn.Read(tmp)
-		if n == 0{
+		n, err := conn.Read(data)
+		if err != nil {
+			fmt.Println("Error reading from connection ", err.Error())
 			break
 		}
-		data = append(data, tmp[:n]...)
+		if n == 0 {
+			break
+		}
+		// msg := string(data[:n])
 		conn.Write([]byte("+PONG\r\n"))
-		fmt.Println("HI")
-		
 	}
 }
 
@@ -27,10 +29,11 @@ func main() {
 	if err != nil {
 		log.Fatalln("Failed to bind to port 6379")
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		log.Fatal("Error accepting connection: ", err.Error())
+	for{
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("Error accepting connection: ", err.Error())
+		}
+		go handleConnection(conn)
 	}
-	handleConnection(conn)
-
 }
