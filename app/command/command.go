36b5919fc1b6ddb15bb9acf58fb9235cd0609b42
Mohammed785 <mohammednady128@gmail.com>
2024-03-14 22:36:52 +0200

    handle SET flags and expiry

--- app/command/command.go
+++ app/command/command.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
@@ -12,13 +13,15 @@ import (
 type Command struct {
 	Name    string
 	Options []string
-	Args    []Arg
+	Args    map[string]string
 }
 
 type Arg struct {
 	Name  string
 	Value string
 }
+
+var SET_OPTIONAL = [][]string{{"nx", "xx"}, {"ex", "px", "exat", "pxat", "keepttl"}}
 
 func NewCommandFromArray(arr resp.RespArray) (*Command, error) {
 	name := strings.ToLower(arr[0].String())
@@ -35,19 +38,50 @@ func NewCommandFromArray(arr resp.RespArray) (*Command, error) {
 		cmd.Options = append(cmd.Options, arr[1].String())
 	case "get":
 		if len(arr) != 2 {
-			return nil, fmt.Errorf("no key provided")
+			return nil, fmt.Errorf("please provide only the key")
 		}
 		cmd.Options = append(cmd.Options, arr[1].String())
 	case "set":
-		if len(arr)<3{
-			return nil,fmt.Errorf("please provide key and value")
+		if len(arr) < 3 {
+			return nil, fmt.Errorf("please provide key and value")
 		}
-		cmd.Options = append(cmd.Options,  arr[1].String(),arr[2].String())
+		cmd.Options = append(cmd.Options, arr[1].String(), arr[2].String())
+		cmd.Args = make(map[string]string)
+		i := 3
+		for i < len(arr) {
+			argLower := strings.ToLower(arr[i].String())
+			if argLower == "nx" || argLower == "xx" || argLower == "get" || argLower == "keepttl" {
+				cmd.Args[argLower] = ""
+				i++
+			} else {
+				if i+1 >= len(arr) {
+					return nil, fmt.Errorf("set '%v' flag needs a value", argLower)
+				}
+				val := arr[i+1].String()
+				_, err := strconv.ParseInt(val, 10, 64)
+				if err != nil {
+					return nil, fmt.Errorf("invalid value '%s' for '%s'", val, argLower)
+				}
+				cmd.Args[argLower] = arr[i+1].String()
+				i += 2
+			}
+		}
+		for _, pairs := range SET_OPTIONAL {
+			exists := 0
+			for i := range pairs {
+				if exists>1 {
+					return nil, fmt.Errorf("you can only set one flag from %v", pairs)
+				}
+				if _, ok := cmd.Args[pairs[i]]; ok {
+					exists++
+				}
+			}
+		}
 	}
 	return cmd, nil
 }
 
-func (c *Command) Execute(conn net.Conn,db *storage.Storage) {
+func (c *Command) Execute(conn net.Conn, db *storage.Storage) {
 	switch c.Name {
 	case "ping":
 		if len(c.Options) == 0 {
@@ -58,10 +92,13 @@ func (c *Command) Execute(conn net.Conn,db *storage.Storage) {
 	case "echo":
 		conn.Write(resp.BulkString([]byte(c.Options[0])).Serialize())
 	case "get":
-		value:=db.Get(c.Options[0])
-		conn.Write(value.Serialize())
+		value, ok := db.Get(c.Options[0])
+		if !ok {
+			conn.Write([]byte("$-1\r\n"))
+		}
+		conn.Write(resp.BulkString([]byte(value)).Serialize())
 	case "set":
-		res := db.Set(c.Options[0],c.Options[1])
+		res := db.Set(c.Options[0], c.Options[1], c.Args)
 		conn.Write(res)
 	}
 }
--- app/storage/storage.go
+++ app/storage/storage.go
@@ -1,26 +1,68 @@
 package storage
 
-import "github.com/codecrafters-io/redis-starter-go/app/resp"
+import (
+	"strconv"
+	"time"
+
+	"github.com/codecrafters-io/redis-starter-go/app/resp"
+)
+
+type entry struct {
+	value string
+	exp   time.Time
+}
 
 type Storage struct {
-	db map[string]string
+	db map[string]entry
 }
 
-func NewStorage()*Storage{
+func NewStorage() *Storage {
 	return &Storage{
-		db: make(map[string]string),
+		db: make(map[string]entry),
 	}
 }
 
-func (s *Storage) Get(key string) resp.BulkString {
-	val, ok := s.db[key]
+func (s *Storage) Get(key string) (string, bool) {
+	value, ok := s.db[key]
 	if !ok {
-		return resp.BulkString{}
+		return "", ok
 	}
-	return resp.BulkString([]byte(val))
+	if !value.exp.IsZero() && value.exp.UnixMilli() < time.Now().UnixMilli() {
+		delete(s.db, key)
+		return "", false
+	}
+	return value.value, ok
 }
 
-func (s *Storage) Set(key, val string) []byte {
-	s.db[key] = val
+func (s *Storage) Set(key, val string, args map[string]string) []byte {
+	newValue := entry{value: val}
+	oldValue, oldExists := s.db[key]
+	returnOld := false
+	for k, v := range args {
+		t, _ := strconv.ParseInt(v, 10, 64)
+		if k == "ex" {
+			newValue.exp = time.Now().Add(time.Second * time.Duration(t))
+		} else if k == "px" {
+			newValue.exp = time.Now().Add(time.Millisecond * time.Duration(t))
+		} else if k == "get" {
+			returnOld = true
+		} else if k == "exat" {
+			newValue.exp = time.Unix(t, 0)
+		} else if k == "pxat" {
+			newValue.exp = time.UnixMilli(t)
+		} else if k == "keepttl" && oldExists {
+			newValue.exp = oldValue.exp
+		} else if (k == "xx" && !oldExists) || (k == "nx" && oldExists) {
+			return []byte("$-1\r\n")
+		}
+	}
+	s.db[key] = newValue
+	if returnOld {
+		if !oldExists{
+			return []byte("$-1\r\n")
+		}else{
+			return resp.BulkString([]byte(oldValue.value)).Serialize()
+		}
+	}
 	return []byte("+OK\r\n")
 }
