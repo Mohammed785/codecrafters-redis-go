d3329db81166e11fb26e150c383f51700d819756
Mohammed785 <mohammednady128@gmail.com>
2024-03-16 21:43:28 +0200

    send handshake 3

--- app/server.go
+++ app/server.go
@@ -61,6 +61,7 @@ func SendHandshake(app *config.App) {
 	m.Write([]byte("*1\r\n$4\r\nping\r\n"))
 	m.Write([]byte(fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n%d\r\n",app.Params.Port)))
 	m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+	m.Write([]byte("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"))
 }
 
 func main() {
